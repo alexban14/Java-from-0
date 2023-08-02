e0579900f0723fca637a654882593c244488a564
Alex Ban <alexbanut10@gmail.com>
2023-08-02 15:01:27 +0300

    introduction of the Java Building Blocks chapter

---
+++ 1.JavaBuildingBlocks/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    // one line comment
+
+    /*
+     * Multi line comment
+     */
+
+    /**
+     * JavaDoc multi-line comment
+     * @author Ban Alexandru
+     */
+
+}
