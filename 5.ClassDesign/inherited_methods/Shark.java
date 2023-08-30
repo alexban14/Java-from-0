03b7741caef1ab597bb4cf50841de4cc383ccc70
Alex Ban <alexbanut10@gmail.com>
2023-08-30 22:54:07 +0300

    inherited methods and overloading vs overriding parent methods

---
+++ 5.ClassDesign/inherited_methods/Shark.java
@@ -0,0 +1,30 @@
+class Fish {
+	protected int size;
+	private int age;
+
+	public Fish(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
+
+public class Shark extends Fish {
+	private int numberOfFins = 8;
+	public Shark(int age) {
+		super(age);
+		this.size = 4;
+	}
+	public void displaySharkDetails() {
+		System.out.print("Shark with age: "+getAge());
+		System.out.print(" and "+size+" meters long");
+		System.out.print(" with "+numberOfFins+" fins");
+
+        // a better way of accesing parent and child class members
+        System.out.print("Shark with age: "+super.getAge());
+        System.out.print(" and "+super.size+" meters long");
+        System.out.print(" with "+this.numberOfFins+" fins");
+	}
+}
