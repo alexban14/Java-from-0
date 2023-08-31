c17f7fed883b5f95f8268f8806fa7bd0cc7967d9
Alex Ban <alexbanut10@gmail.com>
2023-08-31 23:35:33 +0300

    hiding static methods

---
+++ 5.ClassDesign/inherited_methods/HidingStaticMeth.java
@@ -0,0 +1,16 @@
+public class HidingStaticMeth {
+    public class Bear {
+        public static void eat() {
+            System.out.println("Bear is eating");
+        }
+    }
+
+    public class Panda extends Bear {
+        public static void eat() {
+            System.out.println("Panda bear is chewing");
+        }
+        public static void main(String[] args) {
+            Panda.eat();
+        }
+    }
+}
